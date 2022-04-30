b9e8948c78413a1ceab64c4fbfa2146aca2bff19
MY-PC21 <ddmtae@naver.com>
2022-05-01 01:34:00 +0900

    first commit

--- app/src/main/java/com/example/besafe/SettingActivity.kt
+++ app/src/main/java/com/example/besafe/SettingActivity.kt
@@ -1,12 +1,21 @@
 package com.example.besafe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.setting_layout)
+
+        val btn3 = findViewById<Button>(R.id.btn3)
+
+        btn3.setOnClickListener {
+            val intent = Intent(this, addrActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
